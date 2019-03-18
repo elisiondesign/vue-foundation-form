35b5538961745c24f648b57ee9f88f047504fc84
Dan Charvát <dan@elision.design>
2019-03-18 03:03:53 +0100

    Init project with Vue and Zurn foundation.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import "src/assets/style/foundation-sites/settings";
+@import "src/assets/style/foundation-sites/scss/foundation";
+@include foundation-everything;
+
+@include foundation-xy-grid-classes(
+  /* options
+  $base-grid: true,
+  $margin-grid: true,
+  $padding-grid: true,
+  $block-grid: true,
+  $collapse: true,
+  $offset: true,
+  $vertical-grid: true,
+  $frame-grid: true
+  */
+);
+
+// body {
+//   font-size: $titlebar-icon-spacing;
+// }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+<p class="panel">
+  <strong class="show-for-small-only">This text is shown only on a small screen.</strong>
+  <strong class="show-for-medium-up">This text is shown on medium screens and up.</strong>
+  <strong class="show-for-medium-only">This text is shown only on a medium screen.</strong>
+  <strong class="show-for-large-up">This text is shown on large screens and up.</strong>
+  <strong class="show-for-large-only">This text is shown only on a large screen.</strong>
+  <strong class="show-for-xlarge-up">This text is shown on xlarge screens and up.</strong>
+  <strong class="show-for-xlarge-only">This text is shown only on an xlarge screen.</strong>
+  <strong class="show-for-xxlarge-up">This text is shown on xxlarge screens and up.</strong>
+</p>
+
+    <div class="grid-x">
+  <div class="cell">full width cell</div>
+  <div class="cell">full width cell</div>
+</div>
+<div class="grid-x">
+  <div class="cell small-6">6 cells</div>
+  <div class="cell small-6">6 cells</div>
+</div>
+<div class="grid-x">
+  <div class="cell medium-6 large-4">12/6/4 cells</div>
+  <div class="cell medium-6 large-8">12/6/8 cells</div>
+</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style lang="scss">
+
+</style>
