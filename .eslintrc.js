eb098bf2bce9e59ba45266536ead1febafa6477c
Daniel Charvat <xcharva7@fi.muni.cz>
2019-03-22 16:51:07 +0100

    Configure tslint, eslint, editor config to work in unison.

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,13 @@ module.exports = {
         ".ts",
         ".tsx",
         ".vue"
-      ]
+      ],
+      alias: {
+        map: [
+          ['@', './src']
+        ],
+        extensions: ['.ts', '.js', '.jsx', '.json']
+      }
     }
   },
   extends: [
@@ -29,8 +35,11 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-unused-vars': 'warn',
     'max-len': 0,
-      'vue/multiline-html-element-content-newline': 0,
-      'vue/singleline-html-element-content-newline': 0
+    'vue/multiline-html-element-content-newline': 0,
+    'vue/singleline-html-element-content-newline': 0,
+    'arrow-parens': ["error", "always"],
+    'no-unused-vars': 0,
+    'typescript/no-unused-vars': 0
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',
