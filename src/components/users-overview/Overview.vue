b158620c341292f41d3be1a453a3259e1342ac01
Dan Charvát <dan@elision.design>
2019-03-22 09:41:47 +0100

    Add form bits.

---
+++ src/components/users-overview/Overview.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="overview">
+        <h3>Přehled</h3>
+        <table class="table">
+            <tr v-for="(user, index) in users.info" :key="index">
+                <td>{{user.date}}</td>
+                <td>{{user.gender}}</td>
+                <td>{{user.fullName}} {{user.lastName}}</td>
+                <td>Zobrazit</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import {
+  State,
+} from 'vuex-class';
+
+@Component({
+  components: {
+
+  },
+})
+export default class Overview extends Vue {
+  @State('users') users : any;
+}
+</script>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,24 +4,34 @@
     <div class="page">
       <div class="grid-container">
         <h2>Formular</h2>
-        <div class="form grid-x grid-margin-x">
-          <h3 class="cell small-12">Formular</h3>
-          <div class="cell small-12 text-center">
+        <form class="form" @submit="handleSubmit">
+          <h3>Formular</h3>
+          <div>
             Vzorovy button <br>
             Rozsireny label radio buttonu, ktery muze byt delsi
           </div>
 
-          <input
-            type="radio"
-            name="gender"
-            value="female"
-          >Muz</input>
-          <input
-            type="radio"
-            name="gender"
-            value="female"
-          >Zena</input>
-        </div>
+          <label for="firstName">Jméno</label>
+          <input id="firstName" type="text" v-model="data.firstName"/>
+          <label for="lastName">Příjmení</label>
+          <input id="lastName" type="text" v-model="data.lastName"/>
+
+          <input id="male" type="radio" name="gender" value="male" v-model="data.gender"/>
+          <label for="male">Muz</label>
+          <input id="female" type="radio" name="gender" value="female" v-model="data.gender"/>
+          <label for="female">Zena</label>
+
+          <button type="submit">Pokračovat</button>
+
+          <p v-if="errors.length">
+            <b>Vyplňte prosím následující položky</b>
+            <ul>
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+            </ul>
+          </p>
+        </form>
+
+        <overview />
       </div>
     </div>
   </div>
@@ -33,11 +43,57 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import AppTitle from '@/components/Title.vue';
+import Overview from '@/components/users-overview/Overview.vue';
+import {
+  Action,
+} from 'vuex-class';
+
+const namespace: string = 'users';
 
 @Component({
   components: {
     AppTitle,
+    Overview,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+private errors : string[] = [];
+
+  private data = {
+    firstName: null,
+    lastName: null,
+    gender: null,
+  };
+
+  @Action('addEntry', { namespace }) addEntry : any;
+
+  private handleSubmit(e : any) : void {
+    this.checkForm();
+    if (this.errors.length === 0) {
+      this.addEntry(this.data);
+      this.clearForm();
+    }
+    e.preventDefault();
+  }
+
+  private checkForm(): void {
+    this.errors = [];
+
+    if (!this.data.firstName) {
+      this.errors.push('Jméno');
+    }
+    if (!this.data.lastName) {
+      this.errors.push('Příjmení');
+    }
+    if (!this.data.gender) {
+      this.errors.push('Pohlaví');
+    }
+  }
+
+  private clearForm() {
+    this.data.firstName = null;
+    this.data.lastName = null;
+    this.data.gender = null;
+  }
+}
 </script>
