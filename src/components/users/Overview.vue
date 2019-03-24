ac17b4d8b17871ea25997bc7503e531a836da4de
Ondra Pucek <ondra@elisiondesign.cz>
2019-03-24 21:28:32 +0100

    Table styles;

--- src/components/users/Overview.vue
+++ src/components/users/Overview.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="overview">
-    <h3>Přehled</h3>
+    <h3 class="overview__title">Přehled</h3>
     <table class="table">
       <tr
         v-for="(user, index) in users.info"
         :key="index"
       >
-        <td>{{user.date}}</td>
-        <td>{{user.gender}}</td>
-        <td>{{user.name}} {{user.surname}}</td>
-        <td>Zobrazit</td>
+        <td class="table__first">{{user.date}}</td>
+        <td class="table__cell">{{user.gender}}</td>
+        <td class="table__cell">{{user.name}} {{user.surname}}</td>
+        <td class="table__last"><div class="small-button">Zobrazit</div></td>
       </tr>
     </table>
   </div>
 </template>
+
+<style lang="scss">
+@import "src/assets/style/components/overview";
+@import "src/assets/style/components/table";
+@import "src/assets/style/components/small-button";
+</style>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
