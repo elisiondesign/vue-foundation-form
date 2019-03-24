3a271bb526610b7898aaa9052ecf5b3ba6e97d4d
Dan Charvát <dan@elision.design>
2019-03-24 19:49:45 +0100

    Add social network icons.

---
+++ src/components/fa-icons/Twitter.vue
@@ -0,0 +1,19 @@
+<template>
+    <font-awesome-icon :icon="icon" />
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  components: {
+    FontAwesomeIcon,
+  },
+})
+export default class Wallet extends Vue {
+  private icon = faTwitter;
+}
+</script>
--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -3,7 +3,11 @@
     <div class="footer__logo">logo</div>
     <div class="footer__item">Položka</div>
     <div class="footer__item">Položka</div>
-    <div class="footer__social-links">logo</div>
+    <div class="footer__social-links">
+      <fa-facebook />
+      <fa-linked-in />
+      <fa-twitter />
+    </div>
     <div class="footer__lang-swither">EN</div>
   </footer>
 </template>
@@ -11,3 +15,19 @@
 <style lang="scss" scoped>
 @import "src/assets/style/components/footer";
 </style>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import faFacebook from '@/components/fa-icons/Facebook.vue';
+import faLinkedIn from '@/components/fa-icons/LinkedIn.vue';
+import faTwitter from '@/components/fa-icons/Twitter.vue';
+
+@Component({
+  components: {
+    faFacebook,
+    faLinkedIn,
+    faTwitter,
+  },
+})
+export default class Footer extends Vue {}
+</script>
