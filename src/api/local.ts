eb0aece6cf6eb8ef797f8c8a1e54541b14b2de10
Dan Charvát <dan@elision.design>
2019-03-24 20:04:08 +0100

    Fix port issue with axios.

--- src/api/local.ts
+++ src/api/local.ts
@@ -1,12 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import axios, { AxiosPromise } from 'axios';
 
-const baseURL = 'http://localhost:8080';
-
-const api = axios.create({
-  baseURL,
-});
-
 export default function getAsset(path: string): AxiosPromise<any> {
-  return api.get(path);
+  return axios.get(path);
 }
